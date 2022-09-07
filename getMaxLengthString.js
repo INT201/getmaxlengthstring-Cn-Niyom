204cdae4c2e8d91f9e13b0e14a7c08a3e8cec551
Chalunthotn Niyom <Chalunthorn.niyom@gmail.com>
2022-09-07 22:00:39 +0700

    cleancode

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -1,31 +1,24 @@
 const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
-  if(arrayOfString === undefined || arrayOfString=== null)
-  {
+  if (arrayOfString === undefined || arrayOfString === null) {
     return undefined
   }
-  let maxLengthString ;
-  let max=-1;
-  for( index of arrayOfString)
-  {
-   
-    if(index.length >=max)
-    {
-      if(index.length ===max)
-      {
+  let maxLengthString;
+  let max = -1;
+  for (index of arrayOfString) {
+
+    if (index.length >= max) {
+      if (index.length === max) {
         maxLengthString.push(index)
       }
-      else{
-        max=index.length;
-        maxLengthString=[index];
+      else {
+        max = index.length;
+        maxLengthString = [index];
       }
-      
+
     }
   }
   return maxLengthString
 }
-// let test=['hello', 'hi', 'hey']
-let test =['hello', 'hi', 'hey', 'Good Evening', 'Good Morning'];
-console.log(getMaxLengthString(test));
 module.exports = getMaxLengthString
