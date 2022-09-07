ec17641ea6f8b96c28eed3791728a5729e54723e
Chalunthorn Niyom <Chalunthorn.niyom@mail.kmutt.ac.th>
2022-09-07 14:42:08 +0700

    ลอง1

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -1,6 +1,24 @@
 const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
-  //code here
+  if(arrayOfString === undefined || arrayOfString=== null)
+  {
+    return undefined
+  }
+  let maxLengthString;
+  let max=-1;
+  for( index in arrayOfString)
+  {
+    // console.log(arrayOfString[index]);
+    if(arrayOfString[index].length >max)
+    {
+      max=arrayOfString[index].length;
+      maxLengthString=arrayOfString[index];
+    }
+  }
+  return maxLengthString
 }
+// let test=['hello', 'hi', 'hey']
+let test =['hello', 'hi', 'hey', 'Good Evening', 'Good Morning'];
+console.log(getMaxLengthString(test));
 module.exports = getMaxLengthString
