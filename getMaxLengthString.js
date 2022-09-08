422ca46d91a6b5c8332abe63c94a9ee0a344889a
Chalunthotn Niyom <Chalunthorn.niyom@gmail.com>
2022-09-08 23:19:31 +0700

    final final final

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -4,15 +4,18 @@ function getMaxLengthString(arrayOfString) {
   if (arrayOfString === undefined || arrayOfString === null) {
     return undefined
   }
-  let maxLengthString=arrayOfString[0];
-  for (index of arrayOfString) {
-
-    if (index.length >= maxLengthString[0].length) {
-      if (index.length === maxLengthString[0].length) {
-        maxLengthString.push(index)
+  let maxLengthString=[];
+  for (array of arrayOfString) {
+    if(maxLengthString.length === 0)
+    {
+      maxLengthString =[array]
+    }
+    else if (array.length >= maxLengthString[0].length) {
+      if (array.length === maxLengthString[0].length) {
+        maxLengthString.push(array)
       }
       else {
-        maxLengthString = [index];
+        maxLengthString = [array];
       }
 
     }
